5dd4aa4251f4d397e1085427163987373199b5c3
roy1210 <rswkure@gmail.com>
2019-07-07 08:47:49 +0800

    Created firebase functions

---
+++ functions/index.js
@@ -0,0 +1,47 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!");
+});
+
+// when new project / user created
+// doc: new project
+const crateNotification = notification => {
+  return admin
+    .firestore() // (): because add data in `notifications` collection
+    .collection("notifications")
+    .add(notification)
+    .then(doc => console.log("notification added", doc));
+};
+
+exports.projectCreated = functions.firestore // no (): because only ref to documents. It's property
+  .document("projects/{projectId}")
+  .onCreate(doc => {
+    const project = doc.data();
+    const notification = {
+      content: "Added a new PJ",
+      user: `${project.authorFirstName} ${project.authorLastName}`,
+      time: admin.firestore.FieldValue.serverTimestamp()
+    };
+
+    return crateNotification(notification);
+  });
+
+exports.userJoined = functions.auth.user().onCreate(user => {
+  return admin
+    .firestore()
+    .collection("users")
+    .doc(user.uid)
+    .get()
+    .then(doc => {
+      const newUser = doc.data();
+      const notification = {
+        content: "Join the party",
+        user: `${newUser.firstName} ${newUser.lastName}`,
+        time: admin.firestore.FieldValue.serverTimestamp()
+      };
+      return crateNotification(notification);
+    });
+});
